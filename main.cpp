c9f3e8b9a5e987b1c00141287d6080b647419032
TomohiroAoki <a12tomo13@outlook.com>
2023-12-24 21:11:01 +0900

    Add files via upload
    違うやつupしてたのでupしなおし

--- main.cpp
+++ main.cpp
@@ -11,21 +11,20 @@
 
 USB usb(D11, D12, D13, A3, A2); // mosi, miso, sclk, ssel, intr
 BTD device(&usb);
-SPP bluetooth_serial(&device, "Multi-Board-Mini");
+SPP bluetooth_serial(&device, "multi-board-mini");
 
 float x,y;
 
 // Motor pins.
-//PinName pwm_pin = PA_8, forward_pin = PB_0, backward_pin = PB_1;
+PinName pwm_pin = PA_8, forward_pin = PB_0, backward_pin = PB_1;
 
 // Motor2 pins.
-//PinName pwm_pin_2 = PA_9, forward_pin_2 = PB_6, backward_pin_2 = PB_7;
+PinName pwm_pin_2 = PA_9, forward_pin_2 = PB_6, backward_pin_2 = PB_7;
 
 // LED
-//DigitalOut LED_1(PA_6);
+PwmOut led(PA_6);
 
-//IO
-AnalogIn psd(PA_6);
+AnalogIn psd(PA_5);
 
 int main() {
 
@@ -35,42 +34,61 @@ int main() {
   // Start server.
   if (usb.Init() == -1) {
     printf("Failed to start server.\r\n");
-    while (true);
+    while (true)
+      ;
   }
+
   printf("Multi-Board-Console-Server started.\r\n");
 
   // Translator
   MultiBoardConsoleServer::Translator translator;
 
   // Motor driver.
-  //MotorDriver driver(forward_pin, backward_pin, pwm_pin);
-  //MotorDriver driver2(forward_pin_2, backward_pin_2, pwm_pin_2);
+  MotorDriver driver(forward_pin, backward_pin, pwm_pin);
+  MotorDriver driver2(forward_pin_2, backward_pin_2, pwm_pin_2);
 
+  int counter = 0;
 
   // Main process.
   while (1) {
-    // Execute task once.
-    //printf("%d\n\r",dist);
-    usb.Task();
-    //delay(10);
-    dist = (1/psd.read()*7);
+    // printf("%d\n\r",dist);
 
-    // Process by the client requist.
-    if (bluetooth_serial.available()) {
-    //while (received != -1) {  
-      // Pass received data to the translator.
-      translator.append(bluetooth_serial.read());
-      
-      //translator.append(received);
-      //received = bluetooth_serial.read();
+    // Execute task once.
+    usb.Task();
+
+    counter++;
+    if (counter % 100 == 0){
+      dist = (1/psd.read()*7) - 6;
+      // counter = (counter + 1 ) & 0xFF;
+      // bluetooth_serial.write(0);
+      // bluetooth_serial.write(counter);
+      // bluetooth_serial.write(counter ^ 0);
+      bluetooth_serial.write(2);
+      bluetooth_serial.write(dist);
+      bluetooth_serial.write(2 ^ dist); 
+
+      counter = 0;   
+    }
+
+    delay(1);    
     
+    // Process by the client request.
+    while (bluetooth_serial.available() > 0) {
+      int data = bluetooth_serial.read();
+
+      // printf("received: %d\r\n", data);
+
+      // Pass received data to the translator.
+      translator.append(data);
+
       // Get data unit from translator if exists.
       while (translator.has_next()) {
         MultiBoardConsoleServer::DataUnit unit = translator.next();
+
         // Switch processes by the channel.
         switch (unit.channel) {
         case 1:
-          LED_1.write(unit.value);
+          led.write((float)unit.value);
           break;
         case 4:
           x = ((float)(2 * unit.value - 256) / 255.f);
@@ -79,14 +97,10 @@ int main() {
           y = ((float)(2 * unit.value - 256) / 255.f);
           break;
         }
-
-        //driver.drive(MAX_DUTY*(x+y));
-        //driver2.drive(MAX_DUTY*(y-x));
+        // printf("%d: %d\r\n", unit.channel, unit.value);
+        driver.drive(MAX_DUTY*(x+y));
+        driver2.drive(MAX_DUTY*(y-x));
       }
     }
-    bluetooth_serial.write(2);  
-    bluetooth_serial.write(dist);
-    bluetooth_serial.write(2 ^ dist);
-      
   }
 }
